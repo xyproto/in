39a1b300933bc2095ae2696a62f368db2345fdf6
Alexander F. Rødseth <rodseth@gmail.com>
2020-02-23 19:32:33 +0100

    Minor changes

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-const versionString = "in 1.2"
+const versionString = "in 1.2.0"
 
 func main() {
 	if len(os.Args) <= 1 {
@@ -21,20 +21,17 @@ func main() {
 	}
 
 	dir := os.Args[1]
-	err := os.Chdir(dir)
-	if err != nil {
+	if err := os.Chdir(dir); err != nil {
 		if !strings.Contains(err.Error(), "no such file or directory") {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
 		}
 		// Create the missing directory, then try to enter
-		err = os.MkdirAll(dir, 0755)
-		if err != nil {
+		if err = os.MkdirAll(dir, 0755); err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
 		}
-		err = os.Chdir(dir)
-		if err != nil {
+		if err = os.Chdir(dir); err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
 		}
@@ -50,14 +47,12 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	err = cmd.Start()
-	if err != nil {
+	if err := cmd.Start(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
 
-	err = cmd.Wait()
-	if err != nil {
+	if err := cmd.Wait(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
