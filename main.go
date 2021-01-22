e46108292f471917cbfd874855a04dc7cbeac661
Alexander F. Rødseth <rodseth@gmail.com>
2021-01-22 23:39:28 +0100

    2021 update

--- main.go
+++ main.go
@@ -1,57 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
-const versionString = "in 1.2.0"
+const versionString = "in 1.3.0"
 
-func main() {
-	if len(os.Args) <= 1 {
-		fmt.Println("Too few arguments, need a directory as the first argument")
-		os.Exit(1)
+// enter a directory, creating the directory first if needed
+func enter(path string) error {
+	err := os.Chdir(path)
+	if err == nil { // success, no need to create the directory first
+		return nil
 	}
+	// check if the returned PathError has the message "no such file or directory"
+	if pe, ok := err.(*os.PathError); ok && pe.Err.Error() == "no such file or directory" {
+		return err
+	}
+	// create the missing directory
+	if err = os.MkdirAll(path, 0755); err != nil {
+		return err
+	}
+	// enter the directory
+	if err = os.Chdir(path); err != nil {
+		return err
+	}
+	return nil
+}
 
+// run a command
+func run(args ...string) error {
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	if err := cmd.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// the main program
+func program() error {
+	if len(os.Args) <= 1 {
+		return errors.New("too few arguments, need a directory as the first argument")
+	}
 	if os.Args[1] == "-v" || os.Args[1] == "--version" {
 		fmt.Println(versionString)
 		os.Exit(0)
 	}
-
-	dir := os.Args[1]
-	if err := os.Chdir(dir); err != nil {
-		if !strings.Contains(err.Error(), "no such file or directory") {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		// Create the missing directory, then try to enter
-		if err = os.MkdirAll(dir, 0755); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		if err = os.Chdir(dir); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-	}
-
 	if len(os.Args) <= 2 {
-		fmt.Fprintln(os.Stderr, "Too few arguments, need a command after the first argument")
-		os.Exit(1)
+		return errors.New("too few arguments, need a command after the first argument")
 	}
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Start(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	// enter the given directory
+	if err := enter(os.Args[1]); err != nil {
+		return err
 	}
+	// run the given command
+	if err := run(os.Args[2:]...); err != nil {
+		return err
+	}
+	return nil
+}
 
-	if err := cmd.Wait(); err != nil {
+func main() {
+	if err := program(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
