f631e688fddd54a5000eb1180639ef5cedfd952e
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-15 00:42:05 +0200

    Possible fix

--- main.go
+++ main.go
@@ -18,43 +18,30 @@ func enterAndCreate(path string) (bool, error) {
 	if err == nil { // success, no need to create the directory first
 		return false, nil
 	}
-	// for any other error but "no such file or directory", return with an error
-	if pe, ok := err.(*os.PathError); ok && pe.Err.Error() != "no such file or directory" {
+
+	if !os.IsNotExist(err) {
 		return false, err
 	}
+
 	// create the missing directory
 	if err = os.MkdirAll(path, 0755); err != nil {
 		return false, err
 	}
+
 	// enter the directory
 	return true, os.Chdir(path)
 }
 
-// Run a command
+// Run a command within a directory
 func run(directory string, args ...string) error {
-	var err error
 	commandName := args[0]
 
-	absDir, err := filepath.Abs(directory)
-	if err != nil {
-		return err
-	}
-
-	if strings.HasPrefix(commandName, ".") {
-		commandName, err = filepath.Abs(commandName)
-		if err != nil {
-			return err
-		}
-	}
-
 	cmd := exec.Command(commandName, args[1:]...)
-	cmd.Dir = absDir
-
-	cmd.Env = append(cmd.Environ(), "PWD="+absDir)
-	cmd.Env = append(cmd.Environ(), "INDIR="+absDir)
+	cmd.Dir = directory
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	if err := cmd.Start(); err != nil {
 		return err
 	}
@@ -62,42 +49,22 @@ func run(directory string, args ...string) error {
 }
 
 // runInAllMatching runs the given command in all the directories matching the given glob pattern
-func runInAllMatching(pattern, startDir string) error {
+func runInAllMatching(pattern string) error {
 	matches, err := filepath.Glob(pattern)
 	if err != nil {
 		return err
 	}
 
-	// our pattern can match the same parent directory multiple times, so account for that
-	parentDirectories := make(map[string]int, 0)
-	for _, fileName := range matches {
-		directory := fileName
-		stat, err := os.Stat(fileName)
-		if err != nil {
+	seenDirs := map[string]bool{}
+	for _, match := range matches {
+		dir := filepath.Dir(match)
+		if seenDirs[dir] {
+			continue // skip directories we've already seen
+		}
+		seenDirs[dir] = true
+
+		if err := run(dir, os.Args[2:]...); err != nil {
 			return err
-		}
-
-		if stat.IsDir() {
-			directory = filepath.Join(startDir, fileName)
-		} else {
-			directory = filepath.Join(startDir, filepath.Dir(fileName))
-		}
-
-		if count, ok := parentDirectories[directory]; ok {
-			parentDirectories[directory] = count + 1
-		} else {
-			parentDirectories[directory] = 1
-		}
-	}
-
-	for directory := range parentDirectories {
-		// fmt.Println("Running in ", directory)
-		if err := os.Chdir(directory); err != nil {
-			log.Fatalln(err)
-		}
-		// run the given command
-		if err := run(directory, os.Args[2:]...); err != nil {
-			log.Fatalln(err) // exit(1) and skip the deferred function
 		}
 	}
 	return nil
@@ -105,40 +72,45 @@ func runInAllMatching(pattern, startDir string) error {
 
 func main() {
 	if len(os.Args) <= 1 {
-		log.Fatalln("too few arguments, need a directory as the first argument")
+		log.Fatalln("too few arguments, need a directory or glob pattern as the first argument")
 	}
+
 	if os.Args[1] == "-v" || os.Args[1] == "--version" {
 		fmt.Println(versionString)
 		return
 	}
+
 	if len(os.Args) <= 2 {
 		log.Fatalln("too few arguments, need a command after the first argument")
 	}
-	startDir, err := os.Getwd()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	dirName := os.Args[1]
 
-	if strings.Contains(dirName, "*") {
-		if err = runInAllMatching(dirName, startDir); err != nil {
+	dirOrPattern := os.Args[1]
+
+	// if the provided directory includes a glob pattern
+	if strings.Contains(dirOrPattern, "*") {
+		if err := runInAllMatching(dirOrPattern); err != nil {
 			log.Fatalln(err)
 		}
 		return
 	}
 
 	// enter the given directory (and create it, if needed)
-	ok, err := enterAndCreate(dirName)
+	dirCreated, err := enterAndCreate(dirOrPattern)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	// run the given command
-	if err := run(dirName, os.Args[2:]...); err != nil {
-		if ok { // remove the created directory, if it's empty
-			os.Remove(filepath.Join(startDir, dirName))
+	if err := run(dirOrPattern, os.Args[2:]...); err != nil {
+		log.Fatalln(err)
+	}
+
+	// remove the created directory if it's empty, and if it was created by this program
+	if dirCreated {
+		files, err := os.ReadDir(dirOrPattern)
+		if err != nil || len(files) == 0 {
+			os.Remove(dirOrPattern)
 		}
-		log.Fatalln(err) // exit(1) and skip the deferred function
-	} else if ok { // remove the created directory, if it's empty
-		os.Remove(filepath.Join(startDir, dirName))
 	}
 }
+
